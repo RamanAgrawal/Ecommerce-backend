0476b491810593864e1bc9835b66445a7bb07f31
Raman Agrawal <iramanagrawal@gmail.com>
2023-11-07 11:47:40 +0530

    Revert "+logout route controller"
    This reverts commit a00b4267adba2e6134eab6c3ae42e2837a855bea.

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createUser, loginUser, checkUser, logOut } = require('../controller/Auth');
+const { createUser, loginUser, checkUser } = require('../controller/Auth');
 const passport = require('passport');
 
 const router = express.Router();
@@ -7,7 +7,20 @@ const router = express.Router();
 router.post('/signup', createUser)
     .post('/login', passport.authenticate('local'), loginUser)
     .get('/check', passport.authenticate('jwt'), checkUser)
-    .get('/logout',logOut);
+    .get('/logout', (req, res) => {
+        console.log("logout");
+        req.logout((err) => {
+            if (err) {
+                console.error(err);
+            }
+        });
+        res
+            .cookie('jwt', null, {
+                expires: new Date(Date.now()),
+                httpOnly: true,
+            })
+            .sendStatus(200)
+    });
 
 
 exports.router = router;
