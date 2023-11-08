81cbd0ff48df07efdd6ddab0f46d1b2a62757e2e
Raman Agrawal <iramanagrawal@gmail.com>
2023-11-09 00:58:54 +0530

    Existing user check

--- controller/Auth.js
+++ controller/Auth.js
@@ -2,42 +2,50 @@ const { User } = require("../model/User");
 const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 const { sanitizeUser } = require("../utils");
+
+const sessionTime = 1296000000; // 15 days
 exports.createUser = async (req, res) => {
     try {
         // Create a new user instance from the request body
         const salt = crypto.randomBytes(16);
-        const SECRET_KEY = 'secret';
+        const SECRET_KEY = process.env.PASS_SECRET_KEY;
+        const user = await User.findOne({ email: req.body.email }).exec();
+        if (user) {
+            throw new Error('User already exists'); // Throw an error if the user exists
+        } else {
+            crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', async function (err, hashedPassword) {
+                const newUser = new User({ ...req.body, password: hashedPassword, salt });
 
-        crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', async function (err, hashedPassword) {
-            const user = new User({ ...req.body, password: hashedPassword, salt });
-
-            // Save the user to the database and return the response
-            const response = await user.save();
-            req.login(sanitizeUser(response), (err) => {
-                if (err) {
-                    res.status(400).json(err);
-                } else {
-                    const token = jwt.sign(sanitizeUser(user), SECRET_KEY);
-                    res.cookie('jwt', token, { expires: new Date(Date.now() + 3600000), httpOnly: true, sameSite: 'None',secure:true })
-                    // Respond with a 201 status code (Created) and the saved user
-                    res.status(201).json({ id: response.id, role: response.role });
-                    // res.status(201).json(token);
-                }
-            })
+                // Save the user to the database and return the response
+                const response = await newUser.save();
+                req.login(sanitizeUser(response), (err) => {
+                    if (err) {
+                        res.status(400).json(err);
+                    } else {
+                        const token = jwt.sign(sanitizeUser(newUser), SECRET_KEY);
+                        res.cookie('jwt', token, { expires: new Date(Date.now() + sessionTime), httpOnly: true, sameSite: 'None', secure: true })
+                        // Respond with a 201 status code (Created) and the saved user
+                        res.status(201).json({ id: response.id, role: response.role });
+                    }
+                })
+            });
         }
-        );
     } catch (error) {
-        // Log the error for debugging purposes
-        console.log(error);
-
-        // Respond with a 400 status code (Bad Request) and the error message
-        res.status(400).json(error.message);
+        // Handle the "User already exists" error here
+        if (error.message === 'User already exists') {
+            res.status(400).json({ message: 'User already exists Please Signin' });
+        } else {
+           
+            // Respond with a 400 status code (Bad Request) and the error message
+            res.status(400).json(error.message);
+        }
     }
 }
 
+
 exports.loginUser = async (req, res) => {
 
-    res.cookie('jwt', req.user.token, { expires: new Date(Date.now() + 3600000), httpOnly: true ,sameSite:'none',secure:true})
+    res.cookie('jwt', req.user.token, { expires: new Date(Date.now() + sessionTime), httpOnly: true, sameSite: 'none', secure: true })
     res.json(req.user.token)
 }
 
